52c28769e4aceb1b0de6630cec9dc38f678a4074
shailesh-00git <sy468698@gmail.com>
2026-02-01 22:33:27 +0545

    Adde the footer of the mini-project-1

--- 01-mini-project-1/components/Footer.jsx
+++ 01-mini-project-1/components/Footer.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="p-4  border-t l  bg-white">
+      <p className="text-sm text-center text-gray-700">
+        {" "}
+        All rights are reserved
+      </p>
+      <p className=" text-gray-800  text-center">
+        Made with next.js , React and ❤️
+      </p>
+    </footer>
+  );
 }
