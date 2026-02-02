75ac6602f5889a6ba3a3550eecc2925e596dd2a2
shailesh-00git <sy468698@gmail.com>
2026-02-02 22:36:29 +0545

    added the about page

--- 01-mini-project-1/app/about/page.jsx
+++ 01-mini-project-1/app/about/page.jsx
@@ -1,7 +1,94 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="max-w-5xl mx-auto px-4 py-12">
+      <div className="text-center mb-12">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4">About Us</h1>
+        <p className="md:text-3xl text-2xl text-gray-800 mb-8">
+          Learn more about us.What motivate and guide us to work
+        </p>
+        <div className="grid grid-cols-1 gap-x-8 md:grid-cols-2">
+          <div className="porse porse-lg mx-auto">
+            <div className="bg-white rounded-lg shadow-sm border p-8 mb-8">
+              <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+                Our Story
+              </h2>
+              <p className="text-justify mb-4 text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                temporibus, ex itaque illum ipsam excepturi aliquid facilis
+                minus cum architecto laboriosam atque eos assumenda labore alias
+                placeat, laudantium quod corrupti?
+              </p>
+              <p className="text-justify mb-4 text-gray-800">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                temporibus, ex itaque illum ipsam excepturi aliquid facilis
+                minus cum architecto laboriosam atque eos assumenda labore alias
+                placeat, laudantium quod corrupti?
+              </p>
+            </div>
+          </div>
+          <div className="porse porse-lg mx-auto">
+            <div className="bg-white rounded-lg shadow-sm border p-8 mb-8">
+              <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+                Our Vision
+              </h2>
+              <p className="text-justify mb-4 text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                temporibus, ex itaque illum ipsam excepturi aliquid facilis
+                minus cum architecto laboriosam atque eos assumenda labore alias
+                placeat, laudantium quod corrupti?
+              </p>
+              <p className="text-justify mb-4 text-gray-800">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                temporibus, ex itaque illum ipsam excepturi aliquid facilis
+                minus cum architecto laboriosam atque eos assumenda labore alias
+                placeat, laudantium quod corrupti?
+              </p>
+            </div>
+          </div>
+          <div className="porse porse-lg mx-auto">
+            <div className="bg-white rounded-lg shadow-sm border p-8 mb-8">
+              <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+                Our Team
+              </h2>
+              <p className="text-justify mb-4 text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                temporibus, ex itaque illum ipsam excepturi aliquid facilis
+                minus cum architecto laboriosam atque eos assumenda labore alias
+                placeat, laudantium quod corrupti?
+              </p>
+              <p className="text-justify mb-4 text-gray-800">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                temporibus, ex itaque illum ipsam excepturi aliquid facilis
+                minus cum architecto laboriosam atque eos assumenda labore alias
+                placeat, laudantium quod corrupti?
+              </p>
+            </div>
+          </div>
+          <div className="porse porse-lg mx-auto">
+            <div className="bg-white rounded-lg shadow-sm border p-8 mb-8">
+              <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+                Our services
+              </h2>
+              <p className="text-justify mb-4 text-gray-600">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                temporibus, ex itaque illum ipsam excepturi aliquid facilis
+                minus cum architecto laboriosam atque eos assumenda labore alias
+                placeat, laudantium quod corrupti?
+              </p>
+              <p className="text-justify mb-4 text-gray-800">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+                temporibus, ex itaque illum ipsam excepturi aliquid facilis
+                minus cum architecto laboriosam atque eos assumenda labore alias
+                placeat, laudantium quod corrupti?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
