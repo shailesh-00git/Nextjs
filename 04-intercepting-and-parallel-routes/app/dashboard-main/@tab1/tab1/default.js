2747f293a0c2a3c08b54169da291aa1a747468e2
shailesh-00git <sy468698@gmail.com>
2026-02-05 18:37:54 +0545

    Added the unmatched routes

---
+++ 04-intercepting-and-parallel-routes/app/dashboard-main/@tab1/tab1/default.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tab1Page = () => {
+  return <div>Tab1Page</div>;
+};
+
+export default Tab1Page;
---
+++ 04-intercepting-and-parallel-routes/app/dashboard-main/@tab2/tab2/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tab2 = () => {
+  return <div>Tab2</div>;
+};
+
+export default Tab2;
