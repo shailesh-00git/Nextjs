4d2004fa979be521b70269c13ba0c89f61865a9f
shailesh-00git <sy468698@gmail.com>
2026-03-02 21:09:19 +0545

    Added to change favicon

---
+++ 14-middleware-seo-metadata/app/(auth)/login/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  // title: "login",
+  title: {
+    absolute: "login",
+  },
+};
+const LoginPage = () => {
+  return <div>LoginPage</div>;
+};
+
+export default LoginPage;
---
+++ 14-middleware-seo-metadata/app/(auth)/signup/page.js
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "signup",
+  absolute: "",
+};
+export default function SignUp() {
+  return <div>Sign up page</div>;
+}
